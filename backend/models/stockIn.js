1dd655070206259e5b9a0ce03b74d18ba39f4275
JuliusKosman <129964189+JuliusKosman@users.noreply.github.com>
2025-07-01 03:17:28 +0700

    Update stockIn.js

--- backend/models/stockIn.js
+++ backend/models/stockIn.js
@@ -1,5 +1,6 @@
 module.exports = (sequelize, DataTypes) => {
   const StockIn = sequelize.define("StockIn", {
+    freezeTableName: true,
     jumlah: {
       type: DataTypes.INTEGER,
       allowNull: false
