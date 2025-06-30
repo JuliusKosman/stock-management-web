171a14a8ab0708c5bac5c581f637da886cf0c8c0
JuliusKosman <129964189+JuliusKosman@users.noreply.github.com>
2025-07-01 00:24:20 +0700

    Update user.js

--- backend/models/user.js
+++ backend/models/user.js
@@ -4,13 +4,13 @@ module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
     username: {
       type: DataTypes.STRING,
-      unique: true,
+      // unique: true,
       allowNull: false,
   },
 
     email: {
       type: DataTypes.STRING,
-      unique: true,
+      // unique: true,
       allowNull: false,
     },
     password: DataTypes.STRING,
