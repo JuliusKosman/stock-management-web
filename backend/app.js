e3c6de0b0fe0dc911a77c53dc8a44ad6f2cfb201
JuliusKosman <129964189+JuliusKosman@users.noreply.github.com>
2025-07-10 22:01:34 +0700

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -6,37 +6,34 @@ const db = require('./models');
 const authRoutes = require('./routes/authRoutes');
 const barangRoutes = require('./routes/barangRoutes');
 const transaksiRoutes = require('./routes/transaksiRoutes');
+const forecastRoutes = require('./routes/forecastRoutes');
+const arimaRoutes = require('./routes/arimaRoutes');
 
 dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: 'https://stock-management-q7c9weyfv-juliuskosmans-projects.vercel.app',
+  origin: 'https://jcstock.vercel.app',
   credentials: true,
 }));
 
+// Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-// Middleware
-app.use(cors());
-app.use(express.json());
 
 // Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/products', barangRoutes);
 app.use('/api/transaksi', transaksiRoutes);
+app.use('/api/forecast', forecastRoutes);
+app.use('/api/arima', arimaRoutes);
 
 app.get('/', (req, res) => res.send('API Stock Management Running...'));
 
-const forecastRoutes = require('./routes/forecastRoutes');
-app.use('/api/forecast', forecastRoutes);
-
-const arimaRoutes = require('./routes/arimaRoutes');
-app.use('/api/arima', arimaRoutes);
-
+// Scheduler
 require('./schedulers/arimaJob');
 
+// DB Sync & Server Start
 db.sequelize.sync({ alter: true }).then(() => {
   console.log('Database connected & synced.');
 });
